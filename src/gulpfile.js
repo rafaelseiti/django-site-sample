a9264206ef7d4715e84f3557079c8fad8a39c34c
Rafael Otaki <rafael.seiti.otaki@outlook.com>
2017-07-03 23:01:19 -0300

    Arquivos estáticos raiz

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -5,22 +5,22 @@ var gulp = require('gulp'),
 
 gulp.task('uikit.css', function () {
     return gulp.src('node_modules/uikit/dist/css/*.css')
-                .pipe(gulp.dest('msgs/static/uikit/css'));
+                .pipe(gulp.dest('./static/uikit/css'));
 });
 gulp.task('uikit.js', function () {
     return gulp.src('node_modules/uikit/dist/js/*.js')
-    .pipe(gulp.dest('msgs/static/uikit/js'));
+    .pipe(gulp.dest('./static/uikit/js'));
 });
 
 gulp.task('normalize', function () {
     return gulp
             .src('node_modules/normalize.css/*.css')
-            .pipe(gulp.dest('msgs/static/normalize'));
+            .pipe(gulp.dest('./static/normalize'));
 });
 
 gulp.task('jquery', function () {
     return gulp.src('node_modules/jquery/dist/*.js')
-                .pipe(gulp.dest('msgs/static/plugins/jquery'));
+                .pipe(gulp.dest('./static/plugins/jquery'));
 })
 
 gulp.task('default', ['uikit.css', 'uikit.js', 'normalize', 'jquery']);
